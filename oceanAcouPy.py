e3cd7db96dd42daa8c5ed8ccb9919457146a4ec6
Ivana Escobar <ivana@oden.utexas.edu>
2020-10-24 10:55:00 -0500

    sound speed, attenuation

--- oceanAcouPy.py
+++ oceanAcouPy.py
@@ -5,6 +5,75 @@
 # Last Edited: 24 Oct 2020
 
 ### FUNCTIONS ###
+def soundSpeed (t,s,z, lat=0, eqn='mackenzie81'):
+# Inputs:
+#   t : tempurature [degC]
+#   s : salinity [ppt]
+#   z : column depth [m]
+#   lat : latitude [deg]
+#   eqn : mackenzie81, leroy08
+# Returns:
+#   c : seawater sound speed [m/s]    
+    if (eqn=='mackenzie81'):
+        return  1448.96 + 4.59*t - 0.05304*t**2 + 2.374e-4*t**3 + \
+                (1.340 - 0.01025*t)*(s - 35) + 0.01630*z + \
+                1.675e-7*z**2 - 7.139e-13*t*z**3
+    elif (eqn=='leroy08'):
+        return  1402.5  +5*t - 5.44e-2*t**2 + 2.1e-4*t**3 + 1.33*s - \
+                1.23e-2*t*s + 8.7e-5*t**2*s + 1.56e-2*z + \
+                2.55e-7*z**2 - 7.3e-12*z**3 + 1.2e-6*z*(lat-45) - \
+                9.5e-13*t*z**3 + 3e-7*t**2*z + 1.43e-5*s*z
+
+def K (pH):
+# Inputs:
+#   pH : acid-base ratio [1]
+# Returns:
+#   K : logarithmic transformation of pH [1]
+    return 10**(pH-8)
+
+def viscAlpha (f,t,z):
+# Inputs:
+#   f : frequency [Hz]
+#   t : tempurature [degC]
+#   z : column depth [m]
+# Returns:
+#   alpha : attenuation contribution from viscosity [dB/m]
+    return 4.9e-10*exp(z/1.7e4 - t/27) * f**2
+
+def boricAlpha (f,t,s,pH):
+# Inputs:
+#   f : frequency [Hz]
+#   t : tempurature [degC]
+#   s : salinity [ppt]
+#   pH : acid-base ratio [1]
+# Returns:
+#   alpha : attenuation contribution from boric acid [dB/m]
+    fB = 7.8e2*sqrt(s/35)*exp(t/26)
+    if (type(pH) != ndarray and pH > 2): # convert pH to K
+        return 1.06e-4*fB*K(pH)**0.776 * f**2 / (f**2 + fB**2)
+    else: # pH is in units of K already
+        return 1.06e-4*fB*pH**0.776 * f**2 / (f**2 + fB**2)
+
+def magnesiumAlpha (f,t,s,z):
+# Inputs:
+#   f : frequency [Hz]
+#   t : tempurature [degC]
+#   s : salinity [ppt]
+#   z : column depth [m]
+# Returns:
+#   alpha : attenuation contribution from Mg [dB/m]
+    fMg = 4.2e4*exp(t/17)
+    return 5.2e-4*(1+t/43)*s/35*fMg*exp(-z/6e3) * f**2 / \
+            (f**2 + fMg**2)
+
+def alphaTotal (f,t,s,z,pH):
+    return viscAlpha(f,t,z) + boricAlpha(f,t,s,pH) \
+                + magnesiumAlpha(f,t,s,z)
+
+alpha = lambda f : alphaTotal(f,T,S,D,pH)
+av = lambda f : viscAlpha(f,T,D)
+aB = lambda f : boricAlpha(f,T,S,pH)
+aMg = lambda f : magnesiumAlpha(f,T,S,D)
 
 def Theta2 (c1, c2, theta1):
 # Inputs:
