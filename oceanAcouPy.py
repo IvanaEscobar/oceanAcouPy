dd52a6c7097d1bd7195ebff57c948f71428cfb34
Ivana Escobar <ivana@oden.utexas.edu>
2020-11-09 21:35:54 -0600

    array gain

--- oceanAcouPy.py
+++ oceanAcouPy.py
@@ -372,7 +372,20 @@ def beamPattern ( theta, thetaS, f, c, r, noise ):
     steerPhase = bPhase(thetaS, f, c, r)
     beamResponse = []
     for t in theta:
-        lookPhase = phase(t, f, c, r)
+        lookPhase = bPhase(t, f, c, r)
         beamResponse.append( sum( exp(-1j*steerPhase)*\
                                  (exp(1j*lookPhase) + noise) ) )
     return 20*log10( abs(array(beamResponse))/len(r)  )
+
+from scipy.integrate import quad
+
+def beamPatternIntegrand ( theta, thetaS, f, c, r, noise ):
+    steerPhase = bPhase(thetaS, f, c, r)
+    lookPhase = bPhase(theta, f, c, r)
+    return abs( sum( exp(-1j*steerPhase)*\
+                        (exp(1j*lookPhase) + noise) ) / len(r) )**2
+
+def arrayGain (BFIntegral):
+    return 10*log10(2*pi / BFIntegral)
+
+
