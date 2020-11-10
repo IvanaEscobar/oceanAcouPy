7db8ccf268a4502b66e97efed402b340778a85db
Ivana Escobar <ivana@oden.utexas.edu>
2020-11-09 21:01:47 -0600

    beam pattern

--- oceanAcouPy.py
+++ oceanAcouPy.py
@@ -364,3 +364,15 @@ def travelTimes (rayDf, IDlist):
         csi = cDD(rayDf[ii].loc[:,'ray'])
         tau.append(sum(ds/csi[:-1]))
     return array(tau)
+
+def bPhase (theta, f, c, r):
+    return 2*pi*f/c * r * sin(theta * pi / 180)
+
+def beamPattern ( theta, thetaS, f, c, r, noise ):
+    steerPhase = bPhase(thetaS, f, c, r)
+    beamResponse = []
+    for t in theta:
+        lookPhase = phase(t, f, c, r)
+        beamResponse.append( sum( exp(-1j*steerPhase)*\
+                                 (exp(1j*lookPhase) + noise) ) )
+    return 20*log10( abs(array(beamResponse))/len(r)  )
