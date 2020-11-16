0c86ce739f6f93e0ac9d7dc726674b73da9abe41
Ivana Escobar <ivana@oden.utexas.edu>
2020-11-16 14:15:19 -0600

    added data based beamforming

--- oceanAcouPy/beamform.py
+++ oceanAcouPy/beamform.py
@@ -18,11 +18,32 @@ def beamPattern ( theta, thetaS, f, c, r, noise ):
     return 20*log10( abs(array(beamResponse))/len(r)  )
 
 #-------------------------------------------------------------------------------
+def beamPatternData ( theta, dataS, f, c, r, noise ):
+# Same as beam pattern, but with steering data provided
+    beamResponse = []
+    for t in theta:
+        lookPhase = phase(t, f, c, r)
+        beamResponse.append( sum( dataS*\
+                                 (exp(1j*lookPhase) + noise) ) )
+    return 20*log10( abs(array(beamResponse))/len(r)  )
+
+#-------------------------------------------------------------------------------
 def beamPatternIntegrand ( theta, thetaS, f, c, r, noise ):
     steerPhase = bPhase(thetaS, f, c, r)
     lookPhase = bPhase(theta, f, c, r)
     return abs( sum( exp(-1j*steerPhase)*\
                         (exp(1j*lookPhase) + noise) ) / len(r) )**2
+
+#-------------------------------------------------------------------------------
+def beamPatternWindow ( thetaS, theta, f, c, r, noise, window ):
+# window is generated using a SciPy signal function
+    steerPhase = phase(thetaS, f, c, r)
+    beamResponse = []
+    for t in theta:
+        lookPhase = phase(t, f, c, r)
+        beamResponse.append( sum( window*exp(-1j*steerPhase)*\
+                                 (exp(1j*lookPhase) + noise) ) )
+    return 20*log10( abs(array(beamResponse))/ sum(window) )
 
 #-------------------------------------------------------------------------------
 def arrayGain (BFIntegral):
