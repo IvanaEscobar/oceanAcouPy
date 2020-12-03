9ce3ae1e0a20fa37412e7ff48bbbb181d2fa3b0a
Ivana Escobar <ivana@oden.utexas.edu>
2020-12-02 18:43:13 -0600

    pemdas typo

--- oceanAcouPy/detectThreshold.py
+++ oceanAcouPy/detectThreshold.py
@@ -8,4 +8,4 @@ def DT (pd, pfa, m):
     a = log(0.62/pfa)
     b = log(pd/(1-pd))
     return -5*log10(m) + \
-            6.2+ 4.54/sqrt(m+0.44)*log10(a+0.12*a*b+1.7*b) #[dB]
+            (6.2+ 4.54/sqrt(m+0.44))*log10(a+0.12*a*b+1.7*b) #[dB]
