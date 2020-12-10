04051295069cd278aec1eb764386d9460e38de24
Ivana Escobar <ivana@oden.utexas.edu>
2020-12-10 12:49:37 -0600

    total damping apprx

--- oceanAcouPy/bubble.py
+++ oceanAcouPy/bubble.py
@@ -10,5 +10,5 @@ def tsBubble (sigma):
 
 def sigmaBubble (f, z, a):
     fb = 3.25*sqrt(1 + z*0.1) / a
-    deltar = 0.00025*f**(1/3)
+    deltar = 0.0025*f**(1/3)
     return a*a / (((f/fb)**2-1) + deltar*1j)**2
